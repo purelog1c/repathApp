80e2b289bbe1e014c9c50b422f82594e968a3b07
Cenker <34512627+purelog1c@users.noreply.github.com>
2021-10-08 17:29:45 +0300

    Login,Registration Logic

--- app/src/main/java/com/company/repathapp/MainActivity.kt
+++ app/src/main/java/com/company/repathapp/MainActivity.kt
@@ -1,24 +1,89 @@
 package com.company.repathapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextUtils
+import android.text.TextWatcher
+import android.util.Log
+import android.util.Patterns
 import android.widget.Button
-import com.google.firebase.firestore.FirebaseFirestore
-
-
-
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val writeDataDB = DBConnect()
+        val loginBtn: Button = findViewById(R.id.loginButton)
+
+        val emailText: EditText = findViewById(R.id.emailText)
+
+
+        fun isValidEmail(email: String): Boolean {
+            return !TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches()
+        }
+
+        fun String.isValidEmail() =
+            !TextUtils.isEmpty(this) && Patterns.EMAIL_ADDRESS.matcher(this).matches()
+
+
+
+        emailText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {}
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+            }
+            override fun onTextChanged(s: CharSequence, start: Int,
+                                       before: Int, count: Int) {
+                if(!isValidEmail(emailText.text.toString())){
+                    emailText.setTextColor(Color.parseColor("red"))
+                    loginBtn.isClickable = false;
+                    loginBtn.setTextColor(Color.parseColor("#B2B2B2"))
+                    loginBtn.setBackgroundColor(Color.parseColor("#686868"))
+
+                }
+                else{
+                    emailText.setTextColor(Color.parseColor("#757575"))
+                    loginBtn.setTextColor(Color.parseColor("#FFFFFF"))
+                    loginBtn.setBackgroundColor(Color.parseColor("#6200EE"))
+                    loginBtn.isClickable = true;
+                }
+            }
+        })
+
+        loginBtn.isClickable = false;
+        loginBtn.setTextColor(Color.parseColor("#B2B2B2"))
+        loginBtn.setBackgroundColor(Color.parseColor("#686868"))
+
+
+            //GIVE ERROR MESSAGE WHEN FOCUS IS OFF
+
+/*        emailText.setOnFocusChangeListener { view, b ->
+            if(!b) {
+                emailText.setTextColor(Color.parseColor("red"))
+                Log.i("IS it Valid ", "" + isValidEmail(emailText.text.toString()))
+            }
+        }*/
+
+
+
+
+        loginBtn.setOnClickListener {
+        }
+
+
+        //TESTING DATA WRITE
 
         val btnDataWrite: Button = findViewById<Button>(R.id.createData)
 
-        btnDataWrite.setOnClickListener{
+        btnDataWrite.setOnClickListener {
             writeDataDB.setDocument()
         }
-    }
+        // TESTING FINISHED
 
+
+    }
 }
