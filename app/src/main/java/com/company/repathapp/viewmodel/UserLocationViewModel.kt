a83e001e0b27ae5413a5bed70ea381d7820cae9f
Cenker <canbulutcenker@gmail.com>
2021-11-05 18:21:35 +0200

    small touches

--- app/src/main/java/com/company/repathapp/viewmodel/UserLocationViewModel.kt
+++ app/src/main/java/com/company/repathapp/viewmodel/UserLocationViewModel.kt
@@ -1,5 +1,20 @@
 package com.company.repathapp.viewmodel
 
-class UserLocationViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.company.repathapp.model.UserLocationModel
+
+
+class UserLocationViewModel: ViewModel() {
+
+    var userLocationLiveData: MutableLiveData<UserLocationModel>? = null
+
+    fun getUserLocation(): MutableLiveData<UserLocationModel>?{
+
+        if (userLocationLiveData == null){
+            userLocationLiveData = MutableLiveData()
+        }
+        return userLocationLiveData
+    }
 
 }
